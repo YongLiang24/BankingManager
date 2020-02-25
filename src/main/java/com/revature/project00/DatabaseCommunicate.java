42969dcd831daa5daba67d3fe156bc0db385596b
‘yongliang24’ <yongliang24@gmail.com>
2020-02-25 14:06:42 -0500

    added log4j

--- src/main/java/com/revature/project00/DatabaseCommunicate.java
+++ src/main/java/com/revature/project00/DatabaseCommunicate.java
@@ -3,17 +3,29 @@ package com.revature.project00;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class DatabaseCommunicate {
 
+
+	private static final Logger logger = LogManager.getLogger(DatabaseCommunicate.class);
 	public void displayTfLogs() {
-		String tfLog = "select * from account_balance_log";
+		String tfLog = "select * from account_balance_log order by updated_time";
 		CustomerDaoImp ctDao = new CustomerDaoImp();
+		int i=0;
+		
+		logger.error("Display the transaction logs from database");
+		
 		ResultSet result = ctDao.SelectAccount(tfLog);
 		try {
+
+			System.out.println("                   **********Transaction Logs**********\n");
 			while(result.next()) {
+				i++;
 				double transAmount =result.getDouble(3)-result.getDouble(5);
-				System.out.println("                   **********Transaction Logs**********\n");
-				System.out.println(" Account Name: "+result.getString(1)+" | ID: "+result.getInt(2)+" | Trans Amount: $"+transAmount+" | New Balance: $"+result.getDouble(3)+" | Previous Balance: $"+result.getDouble(5)+" | Time: "+result.getString(4)+"\n");
+				System.out.println(i+".Account Name: "+result.getString(1)+"\t ID: "+result.getInt(2)+"\t Amount Transferred: $"+transAmount+"\t Time: $"+result.getString(4)+"\t Previous Balance: $"+result.getDouble(5)+" \t New Balance: "+result.getDouble(3)+"\n");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
--- src/main/java/com/revature/project00/Main.java
+++ src/main/java/com/revature/project00/Main.java
@@ -1,75 +1,11 @@
 package com.revature.project00;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 
-		/*
-		 * 
-		 * 
-		 * String query2 = "select * from Department where DEPT_ID >0";//? is a
-		 * parameter
-		 * 
-		 * ResultSet result = QueryAction.SelectQuery(conn, query2);
-		 * 
-		 * try { while(result.next()) {
-		 * System.out.println(result.getInt("DEPT_ID")+" "+result.getString(2)+" "
-		 * +result.getDouble(3)); } } catch (SQLException e) { //e.printStackTrace(); }
-		 * 
-		 * String query3 ="insert into Department values (12,'home', 4000)";
-		 * 
-		 * QueryAction.InsertUpdateDeleteQuery(conn, query3);
-		 * 
-		 */
-
-		 BankingLogics test = new BankingLogics();
-
-		
-//		  Connection conn =null; 
-//		  PreparedStatement pStatement = null; 
-//		  ResultSet resultSet = null;
-//		  
-//		  try { conn =DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:yong","yongliang", "yl"); } 
-//		  catch (SQLException e) { e.printStackTrace(); } 
-//		  
-//		  String query2 = "select * from employee";
-//		  try {
-//		  pStatement=conn.prepareStatement(query2); 
-//		  resultSet = pStatement.executeQuery(); 
-//		  while(resultSet.next()) {
-//		  System.out.println(resultSet.getString(1)); 
-//		  } } catch (SQLException e) { 
-//			  e.printStackTrace(); }
-		  
-		 
-
-		
-		
-		
-		 // Connection conn = DBConnection.getInstance().getConnect();
-		  
-//		  String query ="select account_name from Customer";
-//		  
-//		  CustomerDaoImp ctDao = new CustomerDaoImp();
-//		  
-//		  ResultSet result =ctDao.SelectAccount(query);
-//		  
-//		  try { while(result.next()) {
-//		  System.out.println(result.getString("account_name")); }; 
-//		  } 
-//		  catch
-//		  (SQLException e) { 
-//			  e.printStackTrace(); 
-//			  }
-		 
-		 
-		 
+		 BankingLogics test = new BankingLogics();		 
 
 	}
 
